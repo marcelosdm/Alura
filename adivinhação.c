bbb5979ee5bbfa7bf48ca4b65216d7c8dde54749
Marcelo Melo <marcelosdm@gmail.com>
2016-04-06 23:05:51 -0300

    Arquivos do curso C1

---
+++ adivinhação.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//#define NUMERO_DE_TENTAVIDAS 5
+
+int main()
+{
+	printf("***************************************\n");
+	printf("Bem vindo ao nosso jogo de adivinhação!\n");
+	printf("***************************************\n");
+
+	int segundos = time(0);
+	srand(segundos);
+
+	int numerogrande = rand();
+
+	int numerosecreto = numerogrande % 100;
+	int chute;
+	int tentativas = 1;
+	double pontos = 1000;
+
+	int acertou = 0;
+
+	int nivel;
+	printf("Qualo nível de dificuldade?\n");
+	printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+	printf("Escolha: ");
+	scanf("%d", &nivel);
+
+	int numerodetentativas;
+	switch(nivel){
+		case 1:
+		numerodetentativas = 20; break;
+		case 2:
+		numerodetentativas = 10; break;
+		default:
+		numerodetentativas = 5; break;
+	}
+
+	for (int i = 1; i <= numerodetentativas; i++)
+	{
+		printf("Tentativa %d\n", tentativas);
+		printf("Qual é o seu chute? ");
+
+		scanf("%d", &chute);
+		printf("Seu chute foi %d!\n", chute );
+
+		if (chute < 0) {
+			printf("Você não pode chutar número negativos!\n");
+			continue;
+		}
+
+		acertou = (chute == numerosecreto);
+		int maior = chute > numerosecreto;
+
+
+		if (acertou){
+			break;		} 
+		
+		else if (maior){
+				printf("Seu chute é maior que o número secreto!\n");
+		}	
+			
+		else {
+				printf("Seu chute é menor que o número secreto!\n");
+		}
+		
+		tentativas++;
+
+		double pontosperdidos = abs(chute - numerosecreto) / (double)2;
+		pontos = pontos - pontosperdidos;
+	}
+
+	printf("Fim de jogo\n");
+
+	if(acertou){
+		printf("Você ganhou!\n");
+		printf("Você acertou em %d tentativas\n", tentativas);
+		printf("Total de pontos: %.1f\n", pontos);
+
+	} else {
+		printf("Você perdeu!Tente de novo!\n");
+	}
+	
+
+}
---
+++ numeros.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+	for(int numero = 1; numero <= 100; numero++){
+		printf("Número %d\n", numero);
+	}
+}
